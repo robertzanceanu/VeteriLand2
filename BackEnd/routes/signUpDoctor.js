5c76cc1217229a6ca7d89a33a1ba0373159c5e8f
AlexandraBeatrice <alexandra5004@yahoo.ro>
2019-05-21 17:29:00 +0300

    packages, procedures, functions

--- BackEnd/routes/signUpDoctor.js
+++ BackEnd/routes/signUpDoctor.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
+var qs = require('qs');
+var oracledb = require('oracledb');
 
-module.exports = function (req, res, oracledb) {
+module.exports = function (req, res) {
     if(req.url === '/signUpDoctor' && req.method === 'GET') {
         var filename = '../View/signUpDoctor.html';
         fs.readFile(filename, function (err,html){
@@ -22,7 +24,48 @@ module.exports = function (req, res, oracledb) {
 
         req.on('end', function () {
             var post = qs.parse(body);
-            console.log(post);
-        });
-    }
+            var firstName = post.firstName;
+            var lastName = post.lastName;
+            var age = post.age;
+            var email = post.email;
+            var password = post.password;
+            var phone = post.phoneNumber;
+
+            async function run()  {
+                try {
+                    connection = await oracledb.getConnection(  {
+                        user          : "STUDENT",
+                        password      : "STUDENT",
+                        connectString : "localhost:1521/xe"
+                      });
+                      console.log("Connected");
+
+                      connection.execute(
+                        `BEGIN 
+                            insertDoctori.insertIntoDoctori(:prenumeDoctor, :numeDoctor, :varstaDoctor, :emailDoctor, :passwordDoctor, :numar_telefonDoctor); 
+                        END;`, {
+                                    prenumeDoctor: firstName,
+                                    numeDoctor: lastName,
+                                    varstaDoctor: age,
+                                    emailDoctor: email,
+                                    passwordDoctor: password,
+                                    numar_telefonDoctor: phone,
+                                },  
+                       function(err, result) {
+                          if (err) {
+                            console.error(err.message);
+                            return;
+                          }
+                          console.log("Succes");
+                        });
+
+                    }
+                catch(err) {
+                    console.log(err);
+                }
+              }
+              run();  
+            
+    });
+}
 }
